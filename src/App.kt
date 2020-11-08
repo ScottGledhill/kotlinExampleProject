d363380e5103bb63d635b5354029913df42d36f3
ScottGledhill <scottgledhill@hotmail.co.uk>
2020-11-08 14:58:38 +0000

    first commit

---
+++ src/App.kt
@@ -0,0 +1,5 @@
+fun main (args: Array<String>) {
+    val myDog = Dog("wank", 899, "idiot")
+    println(myDog.name)
+}
+
---
+++ src/Dog.kt
@@ -0,0 +1,4 @@
+class Dog(val name: String, var weight: Int, val breed: String) {
+
+}
+
